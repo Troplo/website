5bd58ac4e4305bd34c63b8bba23c6d59250258bc
Troplo <troplo@troplo.com>
2024-09-15 16:28:00 +1000

    Set preset to CF pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,12 @@ export default defineNuxtConfig({
       })
     }
   ],
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    },
+    preset: "cloudflare_pages"
+  },
   vite: {
     vue: {
       template: {
